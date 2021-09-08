720cb488fadd9f9b0f0a915857ff7d2252d21326
Ritika Verma <ritika.verma@classplus.co>
2021-09-08 19:18:27 +0530

    Initial Commit

---
+++ EmpManagementSysCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmpManagementSysCoreData
+//
+//  Created by Ritika Verma on 08/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
